e6886838c0a322d90c563241a1071952b7923a97
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-06-23 16:03:23 +0300

    added ui message
    When the user is not at a youtube video page there will be a message on the extension ui

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+import { getCurrentTab } from "./utils";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
@@ -9,4 +11,26 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    // Get the active tab and the search params
+    const activeTab = await getCurrentTab();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    // Check if user is watching a YT video
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo){
+        // Get saved bookmarks
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]): [];
+
+            // TODO: view bookmarks
+        })
+    } else {
+        // This code will run if the user is not on a YT video watch page
+        // and will update the Extension UI in order to inform the user
+        let container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title> This is not a YouTube video page.</div>';
+    }
+});
