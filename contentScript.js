6f88a8a3bcf0f6aa51ad4181a6153998540f7ffd
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-06-23 15:16:50 +0300

    newVideoLoaded fuction made
    basic functionality of the function added

--- contentScript.js
+++ contentScript.js
@@ -7,7 +7,28 @@
 
         if(type === "NEW"){
             currentVideo = videoId;
-            newLoadedVideo();
+            newVideoLoaded();
         }
-    })
+    });
+
+    // newVideoLoaded function
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        
+        if(!bookmarkBtnExists){
+            // Create Bookmark Button
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            // Get YT controls
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+            // Add Bookmark Button to left YT controls
+            youtubeLeftControls.appendChild(bookmarkBtn);
+        }
+    }
 })();
