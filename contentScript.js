7e4886c75ac917a239bf7209e804346d5b556b91
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-06-23 16:58:42 +0300

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -68,6 +68,6 @@ const getTime = t => {
     var date = new Date(0);
     date.setSeconds(t);
     console.log("noSubString" + date.toISOString())
-    console.log("getTime" + date.toISOString().substring(12, 19));
+    console.log("getTime" + date.toISOString().substring(11, 18));
     return date.toISOString().substring(11, 8);
 }
