1fa30537bcaa491fddc8963ff7dc213fbbe7270e
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2022-06-23 16:29:04 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -13,6 +13,7 @@ const addNewBookmark = (bookmarksElement, bookmark) => {
     newBookmarkElement.id = "bookmark-" + bookmark.time;
     newBookmarkElement.className = "bookmark";
     newBookmarkElement.setAttribute("timestamp", bookmark.time);
+    console.log(bookmark.time);
 
     // Add bookmark to ui
     newBookmarkElement.appendChild(bookmarkTitleElement);
